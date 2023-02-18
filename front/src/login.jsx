766b9b40a8d8726d31be66c907a6edaf7c306f36
khs <khs367444@naver.com>
2023-02-18 17:34:57 +0900

    css(4)

---
+++ front/src/login.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './login.css';
+import {useNavigate} from 'react-router-dom'
+import axios from 'axios';
+export default function Login()  {
+    let navigate = useNavigate();
+    return (
+        <div className = 'background'>
+            <button className='login' onClick={()=>{navigate(Login.as_view())}}>signin</button>
+            <h3 className='heading'>Sportygram</h3>
+            <button className='signup' onClick={()=>{navigate('./signup')}}>signup</button>
+            <input type="email" placeholder='email'className='email' />
+            <input type="password"placeholder='password'className='password' />
+            
+        </div>
+        
+        
+    );
+}
+
+
---
+++ front/src/signup.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {useNavigate} from 'react-router-dom'
+import './signup.css'
+
+export default function Signup() {
+    let navigate = useNavigate();
+    return (
+        <div className='lgin'>
+            <input type="email" placeholder='email'className='ema' />
+            <input type="passwode"placeholder='password'className='pass' />
+            <input type="passwode"placeholder='Confirm Password'className='wode' />
+            <button className='signu' onClick={()=>{'./'} }>signup</button>
+        </div>
+    );
+}
+
+
